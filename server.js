f5e491220eda01b668214cfcf3991ba516cf2c20
Zathras <ChampionMike42@gmail.com>
2021-07-20 15:15:42 -0500

    :sparkles: Add working server connection and routes

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-import express from "express";
+const express = require("express");
+const http = require("http");
+const fs = require("fs");
+// const api = require("./public/assets/js/routes.js");
 
 const PORT = 4000;
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html");
+});
+
 app.listen(PORT, () => {
     console.log("server running");
+});
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html");
+});
+
+app.post("/api/notes", (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html");
 });
